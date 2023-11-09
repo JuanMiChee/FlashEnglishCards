7f80f9e8b999a98a41500ec35403c9e500059ea0
Juan <Harringtonjuanc@gmail.com>
2023-11-09 10:46:52 -0500

    Initial Commit

---
+++ FlashEnglishCards/FlashEnglishCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashEnglishCardsApp.swift
+//  FlashEnglishCards
+//
+//  Created by Juan Harrington on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashEnglishCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
